38f51488f48c446f65d1ef0dbd9096ae97207527
m2 Lab <m2lab.post@gmail.com>
2020-11-10 14:39:42 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 'use strict';
 import _ from 'lodash';
+import Tiger  from './utilities';
+// import { NAME as NAME_OF_HAM }  from './utilities';
+// import * as utilities from './utilities';
+// import { NAME, Nijou } from './utilities';
+
+// console.log(utilities.Nijou(3));
+// console.log(NAME_OF_HAM);
+console.log(Tiger.say());
 
 function component() {
   const element = document.createElement('div');
